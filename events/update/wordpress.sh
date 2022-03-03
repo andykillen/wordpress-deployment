d8597ddaffbf3c15afe95ea8152eb5275fd31964
Andrew Killen <andrew@bas-works.com>
2022-03-03 15:31:39 +0100

    adapted to add better scripting after updates

--- events/update/wordpress.sh
+++ events/update/wordpress.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-
-# Get the working directory from the first argument ( $2 is wordpress install type, $3 server type )
+# get the working directory from the first argument
 WORKING_DIRECTORY=$1
+# get the wordpress install type [single/subdirectory/subdomain]
+WORDPRESS_TYPE=$2
+# Server type [apache/nginx/whatever else]
+SERVER_TYPE=$3
+# get the root of the project 
+PROJECT_ROOT_DIRECTORY="$(dirname "$WORKING_DIRECTORY")"
+# copy the .htaccess if apache server
+if [ "$SERVER_TYPE" == "apache" ]; then
+    # check if .htaccess already exists, do not overwrite
+    if [ ! -f "$WORKING_DIRECTORY/.htaccess" ]; then
+        cp "$PROJECT_ROOT_DIRECTORY/files/htaccess-$WORDPRESS_TYPE.txt" "$WORKING_DIRECTORY/.htaccess"
+    fi    
+fi
+# check for robots.txt, create if it does not exist
+if [ ! -f "$WORKING_DIRECTORY/robots.txt" ]; then
+        cp "$PROJECT_ROOT_DIRECTORY/files/robots.txt" "$WORKING_DIRECTORY/robots.txt"
+fi
+
 
 echo $WORKING_DIRECTORY
 # change to doc root of the server (working directory)
